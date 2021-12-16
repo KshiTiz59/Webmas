7ae05660145f9e7bf364b0fcf644e5ab9ea7d4a1
Kshitiz Kumar <92263447+KshiTiz59@users.noreply.github.com>
2021-12-17 02:19:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+document.getElementById('btn').addEventListener('click',
+function(){
+document.querySelector('.t').style.display='none';
+})
+document.getElementById('cancel').addEventListener('click',
+function(){
+document.querySelector('.t').style.display='flex';
+})
+
+//----------------LOGIN----------------
+document.getElementById('button').addEventListener('click',
+function(){
+    document.querySelector('.bg-img').style.display='flex';
+});
+
+document.querySelector('.close').addEventListener('click',
+function(){
+    document.querySelector('.bg-img').style.display='none';
+})
+//--------------LOGIN-----------------
+
+//--------------SIGNUP-------------------
+document.getElementById('button1').addEventListener('click',
+function(){
+    document.querySelector('.bg-img1').style.display='flex';
+});
+
+document.querySelector('.close1').addEventListener('click',
+function(){
+    document.querySelector('.bg-img1').style.display='none';
+})
+//--------------SIGNUP-------------------
